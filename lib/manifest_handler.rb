4ef5e7e4ab6b531d3cbc88e85b34f6107956f708
Cornelius Schumacher <schumacher@kde.org>
2011-06-07 13:02:53 +0200

    Move reading manifests to handler.

--- lib/manifest_handler.rb
+++ lib/manifest_handler.rb
@@ -1,5 +1,17 @@
 class ManifestHandler
 
+  attr_reader :manifests
   
+  def initialize
+    @manifests = Array.new
+  end
+
+  def read_remote
+    Dir.glob( "#{$manifest_dir}/*.manifest" ).sort.each do |filename|
+      File.open filename do |file|
+        manifests.push JSON file.read
+      end
+    end
+  end
 
 end
