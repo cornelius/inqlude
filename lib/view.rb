8e191b04a11fd002cbdd9d497d0ceda7d1ed1c0b
Cornelius Schumacher <schumacher@kde.org>
2011-06-07 13:30:51 +0200

    Generate sub pages for all libraries.

--- lib/view.rb
+++ lib/view.rb
@@ -9,7 +9,7 @@ class View
   end
 
   def create dirname
-    Dir::mkdir dirname unless File.exists? dirname
+    assert_dir dirname
 
     index = template "index"
     engine = Haml::Engine.new index
@@ -17,8 +17,42 @@ class View
     File.open "#{dirname}/index.html", "w" do |file|
       file.puts engine.render( binding )
     end
+
+    library_path = "#{dirname}/libraries/"
+    assert_dir library_path
+
+    engine = Haml::Engine.new template "library"
+
+    manifests.each do |manifest|
+      File.open library_path + manifest["name"] + ".html", "w" do |file|
+        @manifest = manifest
+        file.puts engine.render( binding )
+      end
+    end
+
   end
 
+  def m attr
+    @manifest[ attr ]
+  end
+
+  def link_to_manifest name
+    "<a href=\"libraries/#{name}.html\">#{name}</a>"
+  end
+
+  def manifests
+    if @manifest_handler.manifests.empty?
+      @manifest_handler.read_remote
+    end
+    @manifest_handler.manifests
+  end
+
+  private
+  
+  def assert_dir name
+    Dir::mkdir name unless File.exists? name
+  end    
+  
   def template name
     File.read( File.expand_path( File.dirname( __FILE__ ) +
       "/../view/#{name}.html.haml" ) )
