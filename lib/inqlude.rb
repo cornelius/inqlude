efe9f7acd5c87f80fb8af11dbed914e929c19b7a
Cornelius Schumacher <schumacher@kde.org>
2011-06-13 16:10:13 +0200

    Add tests.

---
+++ lib/inqlude.rb
@@ -0,0 +1,11 @@
+require "rubygems"
+
+require "thor"
+require "json"
+
+require File.expand_path('../cli', __FILE__)
+require File.expand_path('../manifest_handler', __FILE__)
+require File.expand_path('../view', __FILE__)
+require File.expand_path('../distro', __FILE__)
+require File.expand_path('../rpm_manifestizer', __FILE__)
+require File.expand_path('../settings', __FILE__)
---
+++ test/test_helper.rb
@@ -0,0 +1,3 @@
+require File.expand_path('../../lib/inqlude',__FILE__)
+
+require 'test/unit'
