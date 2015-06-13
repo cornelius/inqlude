c142fe256eba5cf6c625f6b93add1a311ea7cfd7
Cornelius Schumacher <schumacher@kde.org>
2015-06-13 12:29:05 +0200

    Check error handling and more commands

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -7,11 +7,50 @@ describe "Command line interface" do
 
   use_given_filesystem(keep_files: true)
 
+  describe "shows error" do
+    it "when command does not exist" do
+      result = run_command(args: ["abc"])
+      expect(result.exit_code).to eq(0)
+      expect(result.stderr).to match("abc")
+      expect(result.stdout.empty?).to be(true)
+    end
+
+    it "when global option does not exist" do
+      result = run_command(args: ["--abc"])
+      expect(result.exit_code).to eq(0)
+      expect(result.stderr).to match("--abc")
+      expect(result.stdout.empty?).to be(true)
+    end
+
+    it "when command option does not exist" do
+      result = run_command(args: ["list", "--abc"])
+      expect(result.exit_code).to eq(0)
+      expect(result.stderr).to match("--abc")
+      expect(result.stdout.empty?).to be(true)
+    end
+  end
+
   describe "help" do
-    it "shows help" do
+    it "shows general help" do
       result = run_command
       expect(result.exit_code).to eq(0)
       expect(result.stdout).to match(/Commands:/)
+      expect(result.stderr.empty?).to be(true)
+    end
+
+    it "shows help for command" do
+      result = run_command(args: ["help", "list"])
+      expect(result.exit_code).to eq(0)
+      expect(result.stdout).to match("inqlude list")
+      expect(result.stderr.empty?).to be(true)
+    end
+  end
+
+  describe "get_involved" do
+    it "shows list of open issues" do
+      result = run_command(args: ["get_involved"])
+      expect(result.exit_code).to eq(0)
+      expect(result.stdout).to match("github.com/cornelius/inqlude/issues")
       expect(result.stderr.empty?).to be(true)
     end
   end
