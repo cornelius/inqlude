a126c9a40718e0b3229edbc554abf8e48e066599
Cornelius Schumacher <schumacher@kde.org>
2015-07-29 13:07:02 +0300

    Add test for global --offline option

--- spec/integration/cli_general_spec.rb
+++ spec/integration/cli_general_spec.rb
@@ -1,6 +1,7 @@
 require_relative "spec_helper"
 
 include CliTester
+include GivenFilesystemSpecHelpers
 
 describe "Command line interface" do
   describe "shows error" do
@@ -14,6 +15,26 @@ describe "Command line interface" do
 
     it "when command option does not exist" do
       expect(run_command(args: ["list", "--abc"])).to exit_with_success("", /--abc/)
+    end
+  end
+
+  describe "global options" do
+    use_given_filesystem
+
+    it "recognizes --offline option" do
+      dir = given_directory do
+        given_directory_from_data("awesomelib")
+      end
+
+      # This doesn't make much sense to a user, but that's what it is right now.
+      # It will go away when we switched to GLI
+      expected_stderr = <<-EOT
+ERROR: "inqlude global" was called with arguments ["list"]
+Usage: "inqlude global"
+      EOT
+
+      expect(run_command(args: ["--offline", "list", "--manifest_dir=#{dir}"]))
+        .to exit_with_success("", expected_stderr)
     end
   end
 end
