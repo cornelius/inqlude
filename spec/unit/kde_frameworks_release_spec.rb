f9e456e94122486ddd49003edd8365ae58a8c161
Cornelius Schumacher <schumacher@kde.org>
2016-10-15 10:15:36 +0200

    Remove unnecessary whitespace

--- spec/unit/kde_frameworks_release_spec.rb
+++ spec/unit/kde_frameworks_release_spec.rb
@@ -1,9 +1,7 @@
 require File.expand_path('../spec_helper', __FILE__)
 
 describe KdeFrameworksRelease do
-
   include GivenFilesystemSpecHelpers
-
   use_given_filesystem
 
   context "given KDE generic manifests" do
@@ -30,47 +28,43 @@ describe KdeFrameworksRelease do
       k = KdeFrameworksRelease.new @manifest_handler
       k.read_generic_manifests
 
-      expect( k.generic_manifests.count ).to eq 2
-      expect( k.generic_manifests[0].name ).to eq "karchive"
-      expect( k.generic_manifests[1].name ).to eq "kservice"
+      expect(k.generic_manifests.count).to eq 2
+      expect(k.generic_manifests[0].name).to eq "karchive"
+      expect(k.generic_manifests[1].name).to eq "kservice"
     end
 
     it "writes release manifests" do
       k = KdeFrameworksRelease.new @manifest_handler
       k.read_generic_manifests
 
-      k.write_release_manifests( "2014-02-01", "4.9.90" )
+      k.write_release_manifests("2014-02-01", "4.9.90")
 
-      manifest_path = File.join( @manifest_dir,
-                                 "karchive/karchive.2014-02-01.manifest" )
-      expect( File.exists? manifest_path ).to be true
+      manifest_path = File.join(@manifest_dir,"karchive/karchive.2014-02-01.manifest")
+      expect(File.exists? manifest_path).to be true
 
-      manifest_path = File.join( @manifest_dir,
-                                 "kservice/kservice.2014-02-01.manifest" )
-      expect( File.exists? manifest_path ).to be true
+      manifest_path = File.join(@manifest_dir,"kservice/kservice.2014-02-01.manifest")
+      expect(File.exists? manifest_path).to be true
 
-      manifest = Manifest.parse_file( manifest_path )
+      manifest = Manifest.parse_file(manifest_path)
       expect(manifest.class).to be ManifestRelease
-      expect( manifest.name ).to eq "kservice"
-      expect( manifest.version ).to eq "4.9.90"
-      expect( manifest.release_date ).to eq "2014-02-01"
+      expect(manifest.name).to eq "kservice"
+      expect(manifest.version).to eq "4.9.90"
+      expect(manifest.release_date).to eq "2014-02-01"
     end
   end
 
   it "creates release manifest from generic manifest" do
-    generic_manifest = Manifest.parse_file(
-      given_file("karchive-generic.manifest") )
+    generic_manifest = Manifest.parse_file(given_file("karchive-generic.manifest"))
     date = "2014-11-06"
     version = "5.4.0"
     release_manifest = KdeFrameworksRelease.create_release_manifest(
       generic_manifest, date, version)
-    expected_json = File.read( given_file("karchive-release-5.4.manifest") )
+    expected_json = File.read(given_file("karchive-release-5.4.manifest"))
     expected_json.chomp! # Remove newline added by File.read
-    expect( release_manifest.to_json ).to eq expected_json
+    expect(release_manifest.to_json).to eq expected_json
   end
 
   it "strips patch release" do
     expect(KdeFrameworksRelease.strip_patch_release("5.4.0")).to eq "5.4"
   end
-
 end
