a367fd1c97da743e439e5c985c731b2a4405d41b
Cornelius Schumacher <schumacher@kde.org>
2019-01-06 15:33:47 +0100

    Update colors

--- .github/labels/cornelius-inqlude-labels.tf
+++ .github/labels/cornelius-inqlude-labels.tf
@@ -9,49 +9,49 @@ resource "github_issue_label" "1181821042a" {
   repository  = "inqlude"
   name        = "Bug"
   description = "Issue in existing functionality"
-  color       = "f9eea7"
+  color       = "000080"
 }
 
 resource "github_issue_label" "1181821042b" {
   repository  = "inqlude"
   name        = "Feature"
   description = "New functionality"
-  color       = "f9eea7"
+  color       = "1034A6"
 }
 
 resource "github_issue_label" "1181821042" {
   repository  = "inqlude"
   name        = "Refactoring"
   description = "Improvement of code without user-visible changes"
-  color       = "f9eea7"
+  color       = "0F52BA"
 }
 
 resource "github_issue_label" "1181821630" {
   repository  = "inqlude"
   name        = "CLI"
   description = "Command Line Interface"
-  color       = "039634"
+  color       = "228B22"
 }
 
 resource "github_issue_label" "1181823572" {
   repository  = "inqlude"
   name        = "Data"
   description = "Inqlude data, including schema and content"
-  color       = "52e59e"
+  color       = "2E8B57"
 }
 
 resource "github_issue_label" "1181820759" {
   repository  = "inqlude"
   name        = "Documentation"
   description = ""
-  color       = "cdffa5"
+  color       = "3CB371"
 }
 
 resource "github_issue_label" "60095012" {
   repository  = "inqlude"
   name        = "Web Site"
   description = "Inqlude web site"
-  color       = "009800"
+  color       = "66CDAA"
 }
 
 resource "github_issue_label" "120813816" {
@@ -86,5 +86,5 @@ resource "github_issue_label" "395340299" {
   repository  = "inqlude"
   name        = "GSoC"
   description = "possible task or project for GSoC"
-  color       = "b60205"
+  color       = "5319e7"
 }
